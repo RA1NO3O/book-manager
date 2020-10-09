95338bbbbf5f71de4c04d12e15b91a11450c5df0
レエイン <RA1NO3O@users.noreply.github.com>
2020-10-09 15:44:45 +0800

    Update ManagerApplication.java

--- src/main/java/com/book/manager/ManagerApplication.java
+++ src/main/java/com/book/manager/ManagerApplication.java
@@ -13,7 +13,7 @@ public class ManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
 		System.out.println("| ---------------------------------------------------------------------------------- |");
-		System.out.println("|                                    Started Success                                 |");
+		System.out.println("|                      成功启动,请访问 http://localhost:8080                          |");
 		System.out.println("| ---------------------------------------------------------------------------------- |");
 	}
 
